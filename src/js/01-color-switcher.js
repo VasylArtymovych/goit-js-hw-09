169e35dcbeebed08a83aa8d9f09cd291335f44c8
VasylArtymovych <art777vasss@gmail.com>
2022-04-19 22:01:51 +0300

    update color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
 
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+};
+
+const TIME_DELAY = 1000;
+let timerId = null;
+
+refs.stopBtn.setAttribute('disabled', 'true');
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(){
+
+    timerId = setInterval(setBgrColor, TIME_DELAY);
+    refs.startBtn.setAttribute('disabled', 'true');
+    refs.stopBtn.removeAttribute('disabled')
+};
+
+function onStopBtnClick(){
+
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute('disabled')
+    refs.stopBtn.setAttribute('disabled', 'true');
+};
+
+
+function setBgrColor(){
+
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+};
+
+function getRandomHexColor() {
+    
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
---
+++ src/js/02-timer.js
@@ -0,0 +1,19 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+const options = {
+    
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    },
+};
+const refs = {
+    // input: document.querySelector('#datetime-picker')
+}
+
+flatpickr('#datetime-picker', options);
+
+let a = flatpickr.currentYear
